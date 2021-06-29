eaabbcfca05a4caf0c59928bbf2e20fbb0007dfd
MartinKist <martin.kistler@live.de>
2021-06-29 12:42:23 +0200

    Create defaults.py

---
+++ defaults.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# (c) 2021 Martin Kistler
+
+from models import NetworkAddress
+
+
+PORT = 5566
+
+OUTGOING = 2
+
+PEERS = {'82.165.67.104': NetworkAddress('82.165.67.104', 5566)}
