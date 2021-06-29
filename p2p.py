d4223eb342a4867ca0254a4f2c697369188d89bf
MartinKist <martin.kistler@live.de>
2021-06-29 21:12:32 +0200

    Update p2p.py

--- p2p.py
+++ p2p.py
@@ -2,21 +2,24 @@
 # (c) 2021 Martin Kistler
 
 import argparse
+import sys
 
 from twisted.logger import globalLogPublisher, ILogObserver, eventAsText
 from zope.interface import provider
+import requests
 
 import defaults
 from client import P2PClient
 
-
+# argument parsing
 parser = argparse.ArgumentParser()
 parser.add_argument('--debug', help='set logging level to debug', action='store_true')
 parser.add_argument('--port', help=f'the port to listen on for incoming connections (default is {defaults.PORT})',
                     type=int, default=defaults.PORT)
 parser.add_argument('--out', help=f'the amount of outgoing connections to maintain (default is {defaults.OUTGOING})',
                     type=int, default=defaults.OUTGOING)
-
+parser.add_argument('--ip', help=f'the IPv4 address your client is reachable under '
+                                 f'(gets curled from "https://icanhazip.com" by default)', type=str, default=None)
 args = parser.parse_args()
 
 
@@ -32,4 +35,17 @@ def simpleObserver(event):
 
 globalLogPublisher.addObserver(simpleObserver)
 
-P2PClient(args.port, args.out).run()
+if args.ip is None:
+    try:
+        res = requests.get('https://icanhazip.com', timeout=5)
+    except:
+        print('Could\'nt get your IPv4 address from https://icanhazip.com.\n'
+              'Set your IPv4 address manually by using the "--ip" flag.')
+        sys.exit()
+
+    ipv4_address = res.text.strip()
+else:
+    ipv4_address = args.ip
+
+
+P2PClient(ipv4_address, args.port, args.out).run()
