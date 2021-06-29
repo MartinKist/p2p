ef8527f7f0a13b1a5052661ae5b489d7ce251049
MartinKist <martin.kistler@live.de>
2021-06-29 12:42:47 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,40 +1,100 @@
 #!/usr/bin/env python3
 # (c) 2021 Martin Kistler
 
-from twisted.internet import reactor
-from twisted.internet.endpoints import TCP4ServerEndpoint
+from twisted.internet import reactor, task
+from twisted.internet.endpoints import TCP4ServerEndpoint, TCP4ClientEndpoint
 from twisted.internet.protocol import Factory
 from twisted.internet.protocol import Protocol
 from twisted.logger import Logger
+from twisted.python.failure import Failure
 
+import defaults
 from models import Version, Message, VerAck, MessageError, GetAddr, NetworkAddress
-from protocols import IncomingPeerFactory
+from protocols import IncomingPeerFactory, OutgoingPeerFactory
 
 
 class P2PClient:
-    def __init__(self, config: dict):
+    log = Logger()
+
+    def __init__(self, port: int, outgoing: int):
         self.version = 0
-        self.port = config['port']
-        self.known_participants = {}
+        self.port = port
+        self.outgoing = outgoing
+
+        # These dicts have the form {str(NetworkAddress): NetworkAddress}
+        self.default_peers = defaults.PEERS
+        self.known_participants = self.default_peers.copy()    # participants of the network this client knows about
+        self.outgoing_cons = {}     # connections made by this client to another client
+        self.incoming_cons = {}     # connections made by another client to this client
+
+    @property
+    def connections(self) -> dict:
+        cons = self.outgoing_cons.copy()
+        cons.update(self.incoming_cons)
+        return cons
 
     def version_compatible(self, peer_version: int) -> bool:
         """
-        Determines if the version of a peer's client is compatible with this client's version.
+        Determine if the version of a peer's client is compatible with this client's version.
         """
         return peer_version == self.version
 
-    def add_participant(self, participant_addr: NetworkAddress):
-        self.known_participants.update({participant_addr.address: participant_addr})
+    def add_participant(self, participant: NetworkAddress):
+        """
+        Adds a new network participant to this client's record of known network participants.
+        :param participant: The NetworkAddress representing the participant to add.
+        """
+        self.known_participants.update({str(participant): participant})
 
-    def remove_participant(self, participant_addr: NetworkAddress):
-        if participant_addr.address in self.known_participants:
-            del self.known_participants[participant_addr.address]
+    def remove_participant(self, participant: NetworkAddress):
+        """
+        Removes a network participant from this client's record of known network participants.
+        :param participant: The NetworkAddress representing the participant to remove.
+        """
+        if participant.address in self.known_participants:
+            del self.known_participants[str(participant)]
 
-    def bootstrap(self):
-        pass
+    def make_new_connection(self):
+        """
+        Try to establish a new outgoing connection to a random known network participant.
+        """
+        for addr, netw_addr in self.known_participants.items():
+            if addr not in self.outgoing_cons:
+                self.connect(netw_addr)
+                break
+        else:
+            for addr, netw_addr in self.default_peers.items():
+                if addr not in self.outgoing_cons:
+                    self.connect(netw_addr)
+                    break
+
+    def connect(self, peer: NetworkAddress):
+        """
+        Try to connect to a new peer.
+        :param peer: The NetworkAddress representing the peer.
+        """
+        endpoint = TCP4ClientEndpoint(reactor, str(peer), peer.port)
+        attempt = endpoint.connect(OutgoingPeerFactory(self))
+        attempt.addErrback(self.handle_connect_errback, peer)
+
+    def handle_connect_errback(self, reason: Failure, format, netw_addr: NetworkAddress):
+        # TODO
+        self.log.failure()
+        if netw_addr in self.known_participants:
+            del self.known_participants[str(netw_addr)]
+
+    def check_connections(self):
+        """
+        Check the health of this client's connections and attempt to make new connections if the desired amount of
+        outgoing connections is not reached.
+        """
+        self.log.info(f'Connected to {len(self.connections)} peers')
+        if len(self.outgoing_cons) < self.outgoing:
+            self.make_new_connection()
 
     def run(self):
-        self.bootstrap()
+        task.LoopingCall(self.check_connections).start(30)
+
         endpoint = TCP4ServerEndpoint(reactor, self.port)
         endpoint.listen(IncomingPeerFactory(self))
 
